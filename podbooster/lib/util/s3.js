65b7329862dcabc646f371dd462fde86cf97f7e8
arellaTV <jeremiah.arella@gmail.com>
2019-01-02 13:37:51 -0800

    Removes reserved aws keywords from env vars

--- podbooster/lib/util/s3.js
+++ podbooster/lib/util/s3.js
@@ -2,9 +2,9 @@
 
 const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    region: process.env.AWS_REGION,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.REGION || process.env.AWS_REGION,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY || process.env.AWS_SECRET_ACCESS_KEY
 });
 
 module.exports = s3;
