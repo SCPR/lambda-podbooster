f91a4e3bb8b059d81041db39387e293e4ba4c31d
arellaTV <jeremiah.arella@gmail.com>
2019-01-03 13:29:21 -0800

    Encodes with the correct sample rate

--- podbooster/lib/util/increase-volume.js
+++ podbooster/lib/util/increase-volume.js
@@ -5,34 +5,45 @@ const request = require('request');
 const uploadToS3 = require('./upload-to-s3');
 const volume = require('pcm-volume');
 
+// NOTE:
+// This script uses 2 passes to properly encode the file.
+// The first stream acquires the sample rate.
+// The second stream applies to the volume transformation and encodes
+// with the sample rate aquired from the previous stream.
+// This isn't ideal because it requires 2 GET requests for every audio file
+
+// TODO: Figure out a way to get the sample rate and continue processing with the same stream.
+
 module.exports = (audioInput) => {
     return new Promise((resolve, reject) => {
         const audio = audioInput || {};
         const newVolume = parseFloat(process.env.VOLUME) || 1.8;
         let volumeStream = new volume();
-        let decoder = new lame.Decoder();
-        let encoder = new lame.Encoder({
-            channels: 1,
-            bitDepth: 16,
-            sampleRate: 44100,
-            bitRate: 64,
-            outSampleRate: 44100,
-            mode: lame.MONO
-        });
-
-        // Perform a GET request to get the original audio stream
-        const originalStream = request(audio.url)
-            .on('error', (err) => {
-                console.error(err);
-                reject(err);
-            })
+        let audioDecoder = new lame.Decoder();
+        let metaDecoder = new lame.Decoder();
 
         // Set the volume to 180% of it's original volume
         volumeStream.setVolume(newVolume);
 
+        // GET the sample rate
+        request(audio.url).pipe(metaDecoder);
+        const sampleRate = await mm.parseStream(metaDecoder, 'audio/mpeg')
+            .then((metadata) => metadata.format.sampleRate)
+            .catch(err => reject(err));
+
+        // GET the original audio stream
+        const originalStream = request(audio.url)
+            .on('error', (err) =>reject(err));
+
         // Decode the mp3 to pcm, apply the new volume, encode back to mp3
-        originalStream.pipe(decoder);
-        decoder.pipe(volumeStream);
+        // with the correct sample rate
+        const encoder = new lame.Encoder({
+            channels: 1,
+            sampleRate: sampleRate,
+        });
+
+        originalStream.pipe(audioDecoder);
+        audioDecoder.pipe(volumeStream);
         volumeStream.pipe(encoder);
 
         // Upload the transcoded file to s3
